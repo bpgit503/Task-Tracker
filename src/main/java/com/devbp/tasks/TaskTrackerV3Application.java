1fe8d6b7e54985fdf7af682f55874573e95a4661
Bren <bpgit503@gmail.com>
2025-03-14 12:22:14 +0100

    first commit

---
+++ src/main/java/com/devbp/tasks/TaskTrackerV3Application.java
@@ -0,0 +1,13 @@
+package com.devbp.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerV3Application.class, args);
+    }
+
+}
