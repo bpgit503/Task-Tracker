bd0c13cb4c12b43dbb774dd5e81ca483159206b3
Bren <bpgit503@gmail.com>
2025-03-19 19:12:21 +0100

    Test: redid list-task-list in Task-List-Controller

--- src/test/java/com/devbp/tasks/controllers/TaskListControllerIT.java
+++ src/test/java/com/devbp/tasks/controllers/TaskListControllerIT.java
@@ -16,9 +16,13 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static com.devbp.tasks.controllers.TaskListController.TASK_LIST_PATH;
+import static com.devbp.tasks.controllers.TaskListController.TASK_LIST_PATH_ID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +42,23 @@ class TaskListControllerIT {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    void testListTaskList() throws Exception {
+        List<TaskList> taskLists = taskListRepository.findAll();
+
+        mockMvc.perform(get(TASK_LIST_PATH)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[*].title", hasItems("Todo List", "Project X")));
+
+
+        assertThat(taskLists).isNotNull();
+        assertThat(taskLists).hasSize(2);
+
+    }
 
     @Test
     void testCreateNewTaskList() throws Exception {
@@ -68,14 +89,22 @@ class TaskListControllerIT {
 
     }
 
-
     @Test
-    void testListTaskList() throws Exception {
-        taskListRepository.deleteAll();
+    void testGetTaskList() throws Exception {
+        UUID taskListId = taskListRepository.findAll().get(0).getId();
 
-        List<TaskList> taskLists = taskListRepository.findAll();
+        MvcResult result = mockMvc.perform(get(TASK_LIST_PATH_ID, taskListId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andReturn();
 
-        assertThat(taskLists).isEmpty();
+        TaskListDto retrieveDto = objectMapper.readValue(result.getResponse().getContentAsString(), TaskListDto.class);
+
+        Optional<TaskList> savedTaskList = taskListRepository.findById(retrieveDto.id());
+        assertThat(savedTaskList).isPresent();
+        assertThat(savedTaskList.get().getTitle()).isEqualTo(retrieveDto.title());
+
 
     }
 }
